9ba4c572d15cff5b4d7760feab4db2fab087d6ca
sneakymining <77371648+sneakymining@users.noreply.github.com>
2021-01-13 03:44:10 +0000

    Update start_miner.bat
    removed commented line.

--- start_miner.bat
+++ start_miner.bat
@@ -8,7 +8,6 @@ setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
-REM IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
 PhoenixMiner.exe -pool ethash.poolbinance.com:8888 -ewal sneakyeth -eworker public -dagrestart 1 -rvram -1 -eres 0
 pause
 
